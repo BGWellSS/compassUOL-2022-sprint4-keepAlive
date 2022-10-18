0099f1a6ecb5dcaccb875591d16e45623467f48a
Wellington Souza <wellington.souza.pb@compasso.com.br>
2022-10-18 13:27:27 -0300

    -- Input cleaning when username or password is invalid

--- js/views/weatherView.js
+++ js/views/weatherView.js
@@ -7,16 +7,6 @@ const weatherNumberDOM = document.querySelector(".weather-number");
 function updateWeather(weather) {
   let cityName = weather.name;
 
-  /**
-   * Não entendi porque a api estava retornando o nome de uma
-   * Instituição beneficente aqui de Belém ao invés do nome da cidade
-   * propriamente dita, analisei as coordenadas e estão corretas
-   * para Belém
-   */
-  if (weather.name == "Curro Velho") {
-    cityName = "Belém";
-  }
-  // FIM - "Correção"
   geoLocationDOM.innerText = `${cityName} - ${weather.sys.country}`;
   let iconName = weather.weather[0].icon;
   containerImgDOM.innerHTML = `<img src="./images/weatherIcons/${iconName}.png">`;
